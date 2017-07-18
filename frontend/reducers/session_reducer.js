27ac36cadff326beb068652a0fb10d08bf79c8f6
boothandrewd <boothandrewd@gmail.com>
2017-07-18 12:06:51 -0700

    Set up session and root reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,25 @@
+import merge from 'lodash';
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_ERRORS
+} from '../actions/session_actions';
+
+const defaultState = {
+  currentUser: null,
+  errors: []
+};
+
+export default (state = defaultState, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, state, { currentUser: action.user });
+
+    case RECEIVE_ERRORS:
+      return merge({}, state, { errors: action.errors });
+
+    default:
+      return state;
+  }
+};
