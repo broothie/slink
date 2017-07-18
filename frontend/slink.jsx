1f6f36e781b562e03caed323789f07299d9c8870
boothandrewd <boothandrewd@gmail.com>
2017-07-18 09:23:14 -0700

    Node packages updated and basic react root set up

---
+++ frontend/slink.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <h1>slink coming even sooner...</h1>,
+    document.getElementById('root')
+  );
+});
