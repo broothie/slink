d712a880e33b7578b3de8e11efa904b85f65f4d7
boothandrewd <boothandrewd@gmail.com>
2017-07-18 11:11:51 -0700

    Fill out application controller for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :signed_in?
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def sign_in?
+    !current_user.nil?
+  end
+
+  def sign_in!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def sign_out!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
 end
