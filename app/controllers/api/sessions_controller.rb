c5d77713243c39ce24f599606dc95d7302bd146c
boothandrewd <boothandrewd@gmail.com>
2017-07-18 11:31:56 -0700

    Get sessions running

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      user_params[:username],
+      user_params[:password]
+    )
+
+    if @user
+      sign_in!(@user)
+      render 'api/users/show'
+    else
+      render json: ['Invalid username/password combination'], status: 401
+    end
+  end
+
+  def destroy
+    if sign_in?
+      sign_out!
+      render json: {}
+    else
+      render json: ['Nobody signed in'], status: 404
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
