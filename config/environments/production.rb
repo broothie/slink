ce27f12f50dbd8a24dab74caa62c042138ea117c
boothandrewd <boothandrewd@gmail.com>
2017-07-20 16:32:52 -0700

    Add config for redis and websockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  subdomain = 'slink-chat'
+  config.web_socket_server_url = "wss://#{subdomain}.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [
+    "http://#{subdomain}.herokuapp.com",
+    "https://#{subdomain}.herokuapp.com"
+  ]
 end
