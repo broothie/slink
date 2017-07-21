ede5a66a62b1522fe7b11ff246cbd8770c322a0b
boothandrewd <boothandrewd@gmail.com>
2017-07-21 14:49:20 -0700

    Make message grabbing N instead of n plus 1

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -1,6 +1,6 @@
 class Api::SubscriptionsController < ApplicationController
   def index
-    @channels = current_user.channels
+    @channels = current_user.channels.includes(:messages)
     render :index
   end
 end
