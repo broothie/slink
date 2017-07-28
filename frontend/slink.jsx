c3022d3f79f184ec911251ff3862de7806b405b9
boothandrewd <boothandrewd@gmail.com>
2017-07-28 15:39:14 -0700

    Remove testing objects from window

--- frontend/slink.jsx
+++ frontend/slink.jsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-// TODO: Remove testing imports
-import { merge } from 'lodash';
-import * as APIUtil from './util/channels_api_util';
-import * as actions from './actions/channel_actions';
-window.APIUtil = APIUtil;
-window.actions = actions;
-window.merge = merge;
-
 document.addEventListener('DOMContentLoaded', () => {
   const store = window.currentUser ? (
     configureStore({ session: { currentUser: window.currentUser } })
@@ -18,9 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
     configureStore()
   );
   delete window.currentUser;
-
-  // TODO: Remove testing window objects
-  window.store = store;
 
   ReactDOM.render(
     <Root store={store}/>,
